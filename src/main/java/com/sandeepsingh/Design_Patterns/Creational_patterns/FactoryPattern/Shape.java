71fb51518fe3822902c740cedcfbbedca737d33a
sandeep79033 <60578012+sandeep79033@users.noreply.github.com>
2020-03-10 18:02:39 +0530

    Add files via upload

---
+++ src/main/java/com/sandeepsingh/Design_Patterns/Creational_patterns/FactoryPattern/Shape.java
@@ -0,0 +1,5 @@
+package com.sandeepsingh.Design_Patterns.Creational_patterns.FactoryPattern;
+
+public interface Shape {
+   void draw();
+}
---
+++ src/main/java/com/sandeepsingh/Design_Patterns/Structural_patterns/ProxyPattern/Image.java
@@ -0,0 +1,5 @@
+package com.sandeepsingh.Design_Patterns.Structural_patterns.ProxyPattern;
+
+public interface Image {
+   void display();
+}
